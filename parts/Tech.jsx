c24ba0d7b56cff2f5dffabc6a7a0e64c060dd7a7
Steven Ray Harvey Falcon <falconsteven25@gmail.com>
2026-02-12 05:41:09 +0800

    Added information for tech.jsx (techs learned & planned)

--- parts/Tech.jsx
+++ parts/Tech.jsx
@@ -1,24 +1,75 @@
 import React from "react";
+import { ReactComponent as HtmlLogo } from "../assets/logos/html.svg";
+import { ReactComponent as CssLogo } from "../assets/logos/css.svg";
+import { ReactComponent as JavascriptLogo } from "../assets/logos/javascript.svg";
+import { ReactComponent as ReactLogo } from "../assets/logos/react.svg";
+import { ReactComponent as PSQLLogo } from "../assets/logos/postgresql.svg";
+import { ReactComponent as TailwindLogo } from "../assets/logos/tailwind.svg";
+import { ReactComponent as RAPILogo } from "../assets/logos/restfulapi.svg";
+import { ReactComponent as NJSLogo } from "../assets/logos/nodejs.svg";
+import { ReactComponent as EJSLogo } from "../assets/logos/expressjs.svg";
+import { ReactComponent as JWTLogo } from "../assets/logos/jwt.svg";
 
 function Tech() {
+  const techsLearned = [
+    {
+      name: "HTML",
+      Icon: HtmlLogo
+    },
+    {
+      name: "CSS",
+      Icon: CssLogo
+    },
+    {
+      name: "JavaScript",
+      Icon: JavascriptLogo
+    },
+    {
+      name: "React",
+      Icon: ReactLogo
+    },
+    {
+      name: "Node.js",
+      Icon: NJSLogo
+    },
+    {
+      name: "Express.js",
+      Icon: EJSLogo
+    }
+  ];
+  const techsLearning = [
+    {
+      name: "PostgreSQL",
+      Icon: PSQLLogo
+    },
+    {
+      name: "Tailwind",
+      Icon: TailwindLogo
+    },
+    {
+      name: "Restful API",
+      Icon: RAPILogo
+    },
+    {
+      name: "JWT Auth",
+      Icon: JWTLogo
+    }
+  ];
+
+  const renderTechs = techs =>
+    techs.map(({ name, Icon }) => (
+      <span key={name} className="tech-item">
+        <Icon className="tech-logo" />
+        {name}
+      </span>
+    ));
   return (
     <section id="techs" className="tech-info">
-      <h1>Techs learned:</h1>
-      <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JavaScript</li>
-        <li>React</li>
-      </ul>
-      <h1>Techs planning to learn/Currently Learning:</h1>
-      <ul>
-        <li>Node.js</li>
-        <li>Express.js</li>
-        <li>Tailwind CSS</li>
-        <li>PostgreSQL</li>
-        <li>Restful APIs</li>
-        <li>JWT Authentication</li>
-      </ul>
+      <h2>Tech Stack</h2>
+      <h3>Techs Learned:</h3>
+      <p className="tech-details">{renderTechs(techsLearned)}</p>
+      <h3>Techs Planned:</h3>
+      <p className="tech-details">{renderTechs(techsLearning)}</p>
     </section>
   );
 }
