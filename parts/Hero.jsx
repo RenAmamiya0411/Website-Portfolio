58dde04ebd0240352afe10d86cb4d40935ef097c
Steven Ray Harvey Falcon <falconsteven25@gmail.com>
2026-02-13 03:11:21 +0800

    Added a Hero section template (Hero.jsx)

---
+++ parts/Hero.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <section className="hero">
+      <h1>Featured Project</h1>
+      <p>Description of said Featured Project</p>
+      <div className="hero-buttons">
+        <a href="/">Demo</a>
+        <a href="/">Github</a>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
