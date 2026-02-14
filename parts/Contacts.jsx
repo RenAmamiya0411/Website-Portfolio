dc81ae72f48bb14e6d20a3c1822f1b1459a64452
Steven Ray Harvey Falcon <falconsteven25@gmail.com>
2026-02-15 06:47:51 +0800

    Added Contact.jsx template

---
+++ parts/Contacts.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Contacts() {
+  return (
+    <section id="contacts">
+      <div className="contacts">
+        <ul className="contact-list">
+          <li className="contact-details">Gmail:</li>
+          <li className="contact-details">Phone Number:</li>
+          <li className="contact-details">LinkedIn:</li>
+        </ul>
+      </div>
+    </section>
+  );
+}
+
+export default Contacts
