c4fd6718f2eea4fb3ffe5bb329dc358b0002e3bc
Steven Ray Harvey Falcon <falconsteven25@gmail.com>
2026-02-15 07:13:25 +0800

    Added icons for contacts

--- parts/Contacts.jsx
+++ parts/Contacts.jsx
@@ -1,17 +1,36 @@
-import React from 'react'
+import React from "react";
+import GmailIcon from "../assets/logos/gmail.svg";
+import GithubIcon from "../assets/logos/github.svg";
+import LinkedInIcon from "../assets/logos/linkedin.svg";
+import PhoneIcon from "../assets/logos/phone.svg";
+import "../App.css";
 
 function Contacts() {
   return (
     <section id="contacts">
       <div className="contacts">
-        <ul className="contact-list">
-          <li className="contact-details">Gmail:</li>
-          <li className="contact-details">Phone Number:</li>
-          <li className="contact-details">LinkedIn:</li>
-        </ul>
+        <h2>Contact Me In:</h2>
+        <div className="contact-details">
+          <p>
+            <img src={GmailIcon} alt="Gmail" className="contact-icons" />
+            Gmail:
+          </p>
+          <p>
+            <img src={LinkedInIcon} alt="LinkedIn" className="contact-icons" />
+            LinkedIn:
+          </p>
+          <p>
+            <img src={GithubIcon} alt="Github" className="contact-icons" />
+            Github:
+          </p>
+          <p>
+            <img src={PhoneIcon} alt="Phone" className="contact-icons" />
+            Phone Number:
+          </p>
+        </div>
       </div>
     </section>
   );
 }
 
-export default Contacts
+export default Contacts;
