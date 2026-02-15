1081d18cc1cf7ebae8846cc5d2e1e24ab6a87cf6
Steven Ray Harvey Falcon <falconsteven25@gmail.com>
2026-02-15 21:29:27 +0800

    Removed  from svgs

--- parts/Contacts.jsx
+++ parts/Contacts.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import GmailIcon from "../assets/logos/gmail.svg";
-import GithubIcon from "../assets/logos/github.svg";
-import LinkedInIcon from "../assets/logos/linkedin.svg";
-import PhoneIcon from "../assets/logos/phone.svg";
+import { ReactComponent as GmailIcon } from "../assets/logos/gmail.svg";
+import { ReactComponent as GithubIcon } from "../assets/logos/github.svg";
+import { ReactComponent as LinkedInIcon } from "../assets/logos/linkedin.svg";
+import { ReactComponent as PhoneIcon } from "../assets/logos/phone.svg";
 import "../App.css";
 
 function Contacts() {
@@ -12,19 +12,19 @@ function Contacts() {
         <h2>Contact Me In:</h2>
         <div className="contact-details">
           <p>
-            <img src={GmailIcon} alt="Gmail" className="contact-icons" />
+            <GmailIcon className="contact-icons" />
             Gmail:
           </p>
           <p>
-            <img src={LinkedInIcon} alt="LinkedIn" className="contact-icons" />
+            <LinkedInIcon className="contact-icons" />
             LinkedIn:
           </p>
           <p>
-            <img src={GithubIcon} alt="Github" className="contact-icons" />
+            <GithubIcon className="contact-icons" />
             Github:
           </p>
           <p>
-            <img src={PhoneIcon} alt="Phone" className="contact-icons" />
+            <PhoneIcon className="contact-icons" />
             Phone Number:
           </p>
         </div>
