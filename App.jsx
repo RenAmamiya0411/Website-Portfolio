19e7bdb9a0916b6173683ef315fc84d471c97902
Steven Ray Harvey Falcon <falconsteven25@gmail.com>
2026-02-15 22:00:28 +0800

    Added design for Contacts.jsx(App.css)

--- App.jsx
+++ App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import "./App.css";
+import "./parts/Projects.css";
+import "./parts/Hero.css";
 import Header from "./parts/Header";
 import About from "./parts/About";
 import Projects from "./parts/Projects";
 import Footer from "./parts/Footer";
+import Tech from "./parts/Tech";
+import Hero from "./parts/Hero";
+import Contacts from "./parts/Contacts";
 
 function App() {
   return (
@@ -11,7 +16,10 @@ function App() {
       <Header />
       <main>
         <About />
+        <Hero />
         <Projects />
+        <Tech />
+        <Contacts />
       </main>
       <Footer />
     </div>
